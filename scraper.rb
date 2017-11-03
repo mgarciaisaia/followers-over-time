cb2557874cca0c3e5602529e92ffe1a484faf853
mgarciaisaia <mgarciaisaia+github@gmail.com>
2017-11-03 11:55:54 -0300

    Manejo de errores

--- scraper.rb
+++ scraper.rb
@@ -6,14 +6,27 @@ require 'mechanize'
 
 agent = Mechanize.new
 
+errors = []
 
 ['cfkargentina', 'anitamontanaro', 'casarosadaar', '678elprograma', 'mauriciomacri', 'CasaRosada', 'presidentpinedo', 'Kicillofok', 'gabimichetti', 'horaciorlarreta', 'SergioMassa', 'danielscioli', 'minsaurralde', 'FuiDespedidoAR'].each do |account|
-  page = agent.get("https://twitter.com/#{account}")
-  time = Time.now
-  followers_link = page.links.detect {|link| link.text.include? 'Followers'}
+  begin
+    page = agent.get("https://twitter.com/#{account}")
+    time = Time.now
+    followers_link = page.links.detect {|link| link.text.include? 'Followers'}
 
-  count = (followers_link.text.match /^\n\s*([^\n]+)/)[1]
-  count_without_commas = count.gsub ',', ''
+    count = (followers_link.text.match /^\n\s*([^\n]+)/)[1]
+    count_without_commas = count.gsub ',', ''
 
-  ScraperWiki.save_sqlite(["account_time"], {"account_time" => "#{account}_#{time.strftime '%Y%m%d_%H%M%S'}", "account" => account, "followers" => count_without_commas, "followers_read" => count, "link_read" => followers_link.text, "time" => time})
+    ScraperWiki.save_sqlite(["account_time"], {"account_time" => "#{account}_#{time.strftime '%Y%m%d_%H%M%S'}", "account" => account, "followers" => count_without_commas, "followers_read" => count, "link_read" => followers_link.text, "time" => time})
+  rescue StandardError => ex
+    errors << ex
+  end
+end
+
+unless errors.empty?
+  errors.each { |error|
+    puts error
+    puts error.backtrace
+  }
+  throw errors.first
 end
